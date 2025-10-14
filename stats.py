fc1ff943cbdf7965e6023fb3c1363fc907d3fcf7
sanguine-hexcraft <hexcraft-apothecary@gmail.com>
2025-10-14 07:24:02 -0600

    adding comments to files

--- stats.py
+++ stats.py
@@ -1,11 +1,17 @@
+# We pass the book text from main().
+# then we split the long string (whole book) so each word is its own string.
+# Then return the length of that list as an integer or count.
 def get_num_words(text):
     return len(text.split())
 
 
+# Again we pass book_words from main() into this function's parameter
 def get_num_characters(text):
+    # Make an empty dictionary
     char_dic = {}
+    # We then lowercase all the strings we passed from main()
     text = text.lower()
-        
+    # Loop through each character(char)     
     for char in text:
         if char not in char_dic:
             char_dic[char] = 0
